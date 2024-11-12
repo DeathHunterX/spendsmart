48cca0d6e3ed384621507f3e8de6d427c33e98e7
Loi Phan <88315858+DeathHunterX@users.noreply.github.com>
2024-11-12 23:17:02 +0700

    add protected routes behavior

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,39 @@
-export { auth as middleware } from "@/auth";
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
+
+const { auth } = NextAuth(authConfig);
+export default auth(async function middleware(
+  req: any
+): Promise<Response | void> {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isSignInPage = nextUrl.pathname === "/sign-in";
+  const isDefaultRedirect = nextUrl.pathname === DEFAULT_LOGIN_REDIRECT;
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (isAuthRoute && isLoggedIn && !isDefaultRedirect) {
+    return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+  }
+
+  if (!isLoggedIn && !isPublicRoute && !isSignInPage) {
+    return Response.redirect(new URL("/sign-in", nextUrl));
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
